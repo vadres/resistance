d6710b531816ff3cbbb7e7a3b07e934fc61c194d
Victor Costa <vpassos10@hotmail.com>
2022-12-22 17:27:24 -0300

    remove jogador

--- src/main/java/com/ennodo/resistence/infra/controller/JogadorController.java
+++ src/main/java/com/ennodo/resistence/infra/controller/JogadorController.java
@@ -5,10 +5,10 @@ import com.ennodo.resistence.infra.service.GameService;
 import com.ennodo.resistence.infra.service.JogadorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +28,12 @@ public class JogadorController {
 	@PostMapping("/novo/{nome}")
 	public ResponseEntity<Void> criarNovo(@PathVariable String nome) {
 		jogadorService.criarJogador(nome);
+		return ResponseEntity.ok().build();
+	}
+
+	@DeleteMapping("{id}")
+	public ResponseEntity<Void> removerJogador(@PathVariable Integer id) {
+		jogadorService.removerJogador(id);
 		return ResponseEntity.ok().build();
 	}
 }
--- src/main/java/com/ennodo/resistence/infra/service/JogadorService.java
+++ src/main/java/com/ennodo/resistence/infra/service/JogadorService.java
@@ -3,9 +3,13 @@ package com.ennodo.resistence.infra.service;
 import com.ennodo.resistence.infra.entity.JogadorJpa;
 import com.ennodo.resistence.infra.repository.JogadorRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class JogadorService {
 	private final JogadorRepository jogadorRepository;
@@ -13,5 +17,15 @@ public class JogadorService {
 	public void criarJogador(String nome) {
 		JogadorJpa jogadorJpa = new JogadorJpa(nome);
 		jogadorRepository.save(jogadorJpa);
+	}
+
+	@Transactional
+	public void removerJogador(Integer id) {
+		try {
+			JogadorJpa jogadorJpa = jogadorRepository.getReferenceById(id);
+			jogadorRepository.delete(jogadorJpa);
+		} catch (Exception e) {
+			log.error(e.getLocalizedMessage(), e);
+		}
 	}
 }
